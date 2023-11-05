884909987fbeeb0f63d34e4140e1a2d44cb9fe54
edwardhwseo <124828951+edwardhwseo@users.noreply.github.com>
2023-11-04 19:17:19 -0500

    Added images to movies
    Added images and displayed movies into cards.

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -52,8 +52,7 @@ dependencies {
     implementation("com.squareup.retrofit2:converter-gson:2.9.0")
     implementation("org.jetbrains.kotlinx:kotlinx-coroutines-android:1.5.2")
     implementation("com.squareup.okhttp3:okhttp:4.11.0")
-
-
+    implementation("io.coil-kt:coil-compose:1.3.0")
     implementation("androidx.core:core-ktx:1.9.0")
     implementation("androidx.lifecycle:lifecycle-runtime-ktx:2.6.1")
     implementation("androidx.activity:activity-compose:1.7.0")
